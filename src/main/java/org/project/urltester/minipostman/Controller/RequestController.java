7a7d9eb902db6fefb910596c973d02ce63f3742c
youceyf11 <Youssef.Ouriniche@emsi-edu.ma>
2025-01-15 11:40:50 +0100

    Initiale commit

--- src/main/java/org/project/urltester/minipostman/Controller/RequestController.java
+++ src/main/java/org/project/urltester/minipostman/Controller/RequestController.java
@@ -5,14 +5,12 @@ import org.project.urltester.minipostman.Requests.Request;
 import org.project.urltester.minipostman.Response.Response;
 import org.project.urltester.minipostman.Service.RequestService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/request")
+@CrossOrigin("*")
 public class RequestController {
 
     private final RequestService requestService;
